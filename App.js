fc620ac32354e36100148c02239dad279cd333fe
nikunj309 <103521892+nikunj309@users.noreply.github.com>
2024-04-30 18:10:19 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,133 @@
+
+//Only the first question has been completed. The second question remains
+
+import { Picker } from "react-native-dropdown-picker";
+import { useState } from "react";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View
+} from "react-native";
+
+const Pattern = () => {
+  const [inputText, setInputText] = useState("");
+  const [randomNum, setRandomNum] = useState([]);
+  const [maxNum, setMaxNum] = useState("");
+
+  const handleInputChange = (text) => {
+    setInputText(text);
+  };
+
+  const handler = () => {
+    // console.log("hello");
+    const num = parseInt(inputText);
+    const array = [];
+    for (let i = 0; i < num; i++) {
+      let j = [];
+        for(let index = 0;  index <= i; index++) {
+        const randomNumber = Math.floor(Math.random() * 100);
+
+        if (!j.includes(randomNumber)) {
+          j.push(randomNumber);
+        }
+      }
+      array.push(j);
+    }
+    setRandomNum(array);
+
+    let maxiNum = array[0][0];
+    for (let i = 0; i < array.length; i++) {
+      {
+        for (let j = 0; j <= array[i].length; j++) {
+          if (array[i][j] > maxiNum) {
+            maxiNum = array[i][j];
+          }
+        }
+      }
+    }
+
+    setMaxNum(maxiNum);
+  };
+  return (
+    <View>
+      <TextInput
+        placeholder="Enter number"
+        onChangeText={handleInputChange}
+        value={inputText}
+      />
+      <Button title="Submit" onPress={handler} />
+      <Text>
+        {randomNum.length > 0
+          ? randomNum.map((j, index) => <Text key={index}>{`${j} \n`}</Text>)
+          : ""}
+      </Text>
+      <Text>
+        max number:
+        {maxNum}
+      </Text>
+    </View>
+  );
+};
+
+const Q2 = () => {
+  const [selectedValue, setSelectedValue] = useState([]);
+
+  const items = [
+    { label: "H", value: "H" },
+    { label: "T", value: "T" },
+  ];
+
+  const handleValueChange = () => {
+
+  }
+
+  return (
+    <View>
+      <Text>Select option:</Text>
+      <Picker
+        selectedValue={null}
+        onValueChange={handleValueChange}
+        items={items}
+        placeholder="Select an item"
+      />
+      <Button onPress={() => setSelectedValue([])}>Reset</Button>
+    </View>
+  );
+};
+
+export default function App() {
+  const [cmp1, setCmp1] = useState(false);
+  const [cmp2, setCmp2] = useState(false);
+
+  const handler = () => {
+    setCmp1(true);
+  };
+  const handler2 = () => {
+    setCmp2(true);
+  };
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={handler}>
+        <Text>h1</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={handler2}>
+        <Text>h2</Text>
+      </TouchableOpacity>
+
+      {cmp1 && <Pattern />}
+      {cmp2 && <Q2 />}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
